d95ae22a65ab55ca763ae03ee3e7c5e9ea38c870
Siglo-R <130428190+Siglo-R@users.noreply.github.com>
2023-05-01 12:46:04 +0800

    Homework part1 Output

---
+++ day2/PrimeNumber.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+class PrimeNumber {
+    public static void main(String[] args) {
+        Scanner scanner= new Scanner(System.in);
+        int ent_num = scanner.nextInt();
+        int ctr=0;
+        if(ent_num <=1){
+            System.out.println("FALSE");
+        }
+        else {
+            for (int i = 2; i <= ent_num; i++) {
+                if (ent_num % i == 0){
+                    ctr++;
+                }
+            }
+            if (ctr<=1){
+                System.out.println("TRUE");
+            }
+            else {
+                System.out.println("FALSE");
+            }
+        }
+    }
+}
