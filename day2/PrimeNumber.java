4a138fa6c2cd14deb896214040e8a655d0c33edf
Siglo-R <130428190+Siglo-R@users.noreply.github.com>
2023-05-02 11:20:26 +0800

    changed value of print statement TRUE and FALSE to boolean

--- day2/PrimeNumber.java
+++ day2/PrimeNumber.java
@@ -5,8 +5,11 @@ class PrimeNumber {
         Scanner scanner= new Scanner(System.in);
         int ent_num = scanner.nextInt();
         int ctr=0;
+        boolean FALSE=false;
+        boolean TRUE=true;
         if(ent_num <=1){
-            System.out.println("FALSE");
+
+            System.out.println(FALSE);
         }
         else {
             for (int i = 2; i <= ent_num; i++) {
@@ -15,10 +18,10 @@ class PrimeNumber {
                 }
             }
             if (ctr<=1){
-                System.out.println("TRUE");
+                System.out.println(TRUE);
             }
             else {
-                System.out.println("FALSE");
+                System.out.println(FALSE);
             }
         }
     }
