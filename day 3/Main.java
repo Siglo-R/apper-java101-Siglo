d7a0f39f4fef3b4cc37219e17dca360cd1888e1c
Siglo-R <130428190+Siglo-R@users.noreply.github.com>
2023-05-05 11:58:12 +0800

    CategoryEnum Code for switch cases added

--- day 3/Main.java
+++ day 3/Main.java
@@ -37,4 +37,27 @@ class Main {
         public static void displayFirstElementInArray() {
             
         }
+    //Added Switch Statement lessons using CategoryENum
+    /*
+class CategoryEnum {
+
+    public static void main(String[] args){
+
+        Category chosenCategory= Category.MEN;
+
+        switch (chosenCategory){
+            case MEN: System.out.println("arf arf");break;
+            case WOMEN: System.out.println("meow meow"); break;
+            case KID: System.out.println("meeeh"); break;
+            case GRANDMA: System.out.println("mooo"); break;
+            case GRANDPA: System.out.println("luh"); break;
+            default: System.out.println("not your family please try again");
+    }
+
+    }
+    public enum Category{
+        MEN, WOMEN, KID, GRANDMA, GRANDPA
+    }
+    */
+}
 }
