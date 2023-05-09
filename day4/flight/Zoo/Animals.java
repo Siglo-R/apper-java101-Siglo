fc953f6faad624c78d38880f9601da0196dc8f21
Siglo-R <130428190+Siglo-R@users.noreply.github.com>
2023-05-09 12:50:42 +0800

    Added Zoo package in day 4

---
+++ day4/flight/Zoo/Animals.java
@@ -0,0 +1,10 @@
+package Zoo;
+//values as place holder
+public class Animals {
+         String animalsName = "Cow";
+         String animalsType = "mammal";
+         String animalsFood = "Grass";
+         String animalsSound = "moo";
+         boolean animalsCanChew = true;
+
+}
---
+++ day4/flight/Zoo/Visitors.java
@@ -0,0 +1,12 @@
+package Zoo;
+//values as place holder
+public class Visitors {
+    public String visitorName= "Ian Panganiban";
+    private int visitorAge=42;
+
+    public int getVisitorAge(){
+        return visitorAge;
+    };
+
+
+}
